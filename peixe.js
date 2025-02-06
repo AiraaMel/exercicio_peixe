5d761dd49661fa1a6b33b15e6ae21a0ca49dda60
Aira Mel <aira.souza@sou.inteli.edu.br>
2025-02-06 09:34:00 -0300

    Adicionado anotações

--- peixe.js
+++ peixe.js
@@ -15,25 +15,42 @@ var game = new Phaser.Game(config);
 var linguado;
 
 function preload (){ 
+   
+    // carregar o fundo 
     this.load.image('mar', 'assets/bg_azul-claro.png');
+
+    // carregar o logo
     this.load.image('logo', 'assets/logo-inteli_branco.png');
+    
+    // carregar o peixe
     this.load.image('peixe', 'assets/peixe/peixe_amarelo.png');
+    
+    // carregar o elemento
     this.load.image('elemento', 'assets/novo_elemento.png');
 }
 
 function create (){
+    
+    // adicionar o fundo na tela
     this.add.image(400, 300, 'mar');
+
+    // adicinar o elemento na tela
     this.add.image(400, 300, 'elemento');
+
+    // adicionar o logo na tela
     this.add.image(400, 525, 'logo').setScale(0.5);
 
-
+    //guardar o peixe na variável linguado
     linguado = this.add.image(400, 300, 'peixe')
     
+    // transformando a variável linguado
     linguado.setFlip(true, false);
 }
 
 
 function update () {
+    
+    // movimentar o peixe
     linguado.x = this.input.x;
     linguado.y = this.input.y;
 }
